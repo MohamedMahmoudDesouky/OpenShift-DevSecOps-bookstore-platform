1c373b759da0afc297ba6fe5abfa1f7d2a2d829e
AymanKamell <aymankamel20k@gmail.com>
2026-01-14 17:06:32 +0200

    Delete validate-security.sh

--- validate-security.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -uo pipefail
-
-NAMESPACE="bookstore"
-
-echo "===== Runtime Security Validation ====="
-
-kubectl get pods -n "$NAMESPACE"
-kubectl get networkpolicy -n "$NAMESPACE"
